248d87cbead94cf2ae68869779e31ca8f5247ad1
Anselm Stordeur <stordeur@campus.tu-berlin.de>
2016-07-03 15:42:10 +0200

    Adds feature to format tabular with lines Deletes docs files Adds further testing

--- matrix.js
+++ matrix.js
@@ -1,14 +1,17 @@
 const assert = require('assert');
 
 /**
- * @description takes an array as matrix and converts it to an latex Tabular.
- * @param matrix - Data Array
- * @param width - width or breadth of the matrix. Array get cut depending on this param.
- * @param title - the caption of the Latex Tabular
+ * @description Takes an array as matrix and converts it to an latex Tabular.
+ * @param {object} matrix - Data Array -> gets sliced in rows of the tabular depending on the width parameter
+ * @param {int} width - width or breadth of the matrix. Array gets cut depending on this parameter
+ * @param {object} [options] - optional options for formatting the tabular
+ * @param {string} [options.caption] - a nice caption for the tabular
+ * @param {boolean[]} [options.hLines] - an array which elements say if a horizontal line should be printed in top of a row. To get a fully closed tabular the array has to be filled with (rows + 1) true statements.
+ * @param {boolean[]} [options.vLines] - an array which elements say if a vertical line should be printed left of a column. To get a fully closed tabular the array has to be filled with (columns + 1) true statements.
  * @returns {string} - Latex Tabular
  */
 
-function formattedTabular(matrix, width, title) {
+function formattedTabular(matrix, width, options) {
 
   // check for valid matrix
   assert.ok(matrix, 'Matrix is undefined');
@@ -24,15 +27,36 @@ function formattedTabular(matrix, width, title) {
     throw new Error('width is bigger than matrix');
   }
 
-  let head = '\\begin{tabular}{' + 'l'.repeat(width) + '}\n';
-
+  let collumnHead = 'l'.repeat(width);
   let caption = '';
-  if(typeof title === 'string' && title.length > 0){
-    caption = '\\multicolumn{' + width + '}{c}{' + title + '} \\\\ \\hline\n';
-  }
+  let row = 0;
 
   let data = '';
   let foot = '\\end{tabular}\n';
+
+  if(typeof options === 'object'){
+    // caption
+    if(Object.keys(options).indexOf('caption') >= 0 && typeof options.caption === 'string'){
+      caption = '\\multicolumn{' + width + '}{c}{' + options.caption + '} \\\\ \\hline\n';
+    }
+    // vLines
+    if(Array.isArray(options.vLines)){
+      collumnHead = '';
+      for(let i = 0; i <= width; i++){
+        collumnHead += options.vLines[i] ? '|' : '';
+        if(i < width){
+          collumnHead += 'l';
+        }
+      }
+    }
+    // for hLines fitst row
+    if(Object.keys(options).indexOf('hLines') >= 0 && Array.isArray(options.hLines) && options.hLines[row]){
+      caption += caption.length > 0 ? '' : '\\hline\n'; // if no caption and hLine in first line
+      row++;
+    }
+  }
+
+  let head = '\\begin{tabular}{' + collumnHead + '}\n';
 
   matrix.forEach(function (item, index, arr) {
     // sanitize item
@@ -48,10 +72,19 @@ function formattedTabular(matrix, width, title) {
     let itemEnd = '';
 
     // if item is last of row
-    if((index + 1) % width === 0){
-      lineEnd = (((index + 1) < arr.length) ? ' \\\\\n' : '\n'); // but not last of matrix
+    if((index + 1) % width === 0 || index === (arr.length - 1)){
+      if(typeof options === 'object' && Array.isArray(options.hLines) && options.hLines[row]){
+        lineEnd += ' \\\\ \\hline'
+      }else {
+        lineEnd = (((index + 1) < arr.length) ? ' \\\\' : ''); // but not last of matrix
+      }
+      lineEnd += '\n';
+      row++;
     }else {
       itemEnd = ' & ';
+    }
+    if(index + 1 === arr.length && (index + 1) % width !== 0){
+      itemEnd = ' &';
     }
     data += item + itemEnd + lineEnd;
   });
---
+++ test/matrix.js
@@ -0,0 +1,120 @@
+const assert = require('assert');
+const matrix = require('../matrix');
+
+"use strict";
+
+describe('Converts Matrix in a Latex Tabular', function () {
+
+  describe('Test conversion in formatted Tabular', function () {
+
+    it('should respond with a valid string on valid input', function () {
+      assert.strictEqual(typeof matrix.formattedTabular([2, 2], 1), 'string');
+    });
+
+    it('should respond with a valid string on valid input with crazy types in it', function () {
+      assert.strictEqual(typeof matrix.formattedTabular([null, undefined, NaN, 0, true, 0.0, { key: '2', value: 2 }, [2,1], new Date()], 3), 'string');
+    });
+
+    it('should respond with a valid string on big input (big Array with 65536 Elements)', function () {
+      let bigArray = new Array(65536).fill(1);
+      assert.strictEqual(typeof matrix.formattedTabular(bigArray, 120), 'string');
+    });
+
+    it('should throw error if matrix is undefined', function () {
+      try {
+        matrix.formattedTabular(null, 2);
+        throw new Error('no Error thrown');
+      } catch (err) {
+        assert.strictEqual(err.message, 'Matrix is undefined');
+      }
+    });
+
+    it('should throw error if matrix is empty', function () {
+      try {
+        matrix.formattedTabular([], 1);
+        throw new Error('no Error thrown');
+      }catch (err){
+        assert.strictEqual(err.message, 'Matrix is empty');
+      }
+    });
+
+    it('should throw error if width is undefined', function () {
+      try {
+        matrix.formattedTabular([1, 2], NaN);
+        throw new Error('no Error thrown');
+      }catch (err){
+        assert.strictEqual(err.message, 'width is undefined');
+      }
+    });
+
+    it('should throw error if width is bigger than matrix', function () {
+      try {
+        matrix.formattedTabular([1, 2], 3);
+        throw new Error('no Error thrown');
+      }catch (err){
+        assert.strictEqual(err.message, 'width is bigger than matrix');
+      }
+    });
+
+    it('should respond with a well formatted Tabulor on valid input', function () {1
+      assert.strictEqual(matrix.formattedTabular([0, 0, 0, 0], 2),
+`\\begin{tabular}{ll}
+0 & 0 \\\\
+0 & 0
+\\end{tabular}\n`, 'simple 4x4 number matrix');
+    });
+
+    it('should respond with well formatted types on valid crazy input', function (){
+      assert.strictEqual(matrix.formattedTabular([null, undefined, NaN, 0, true, 0.0, { key: '2', value: 2 }, [2,1], 'string'], 3),
+`\\begin{tabular}{lll}
+- & - & - \\\\
+0 & true & 0 \\\\
+{"key":"2","value":2} & [2,1] & string
+\\end{tabular}\n`, 'matrix with crazy types');
+    });
+
+    it('should respond with a nice caption for the tabulor on valid input with caption option', function () {
+      assert.strictEqual(matrix.formattedTabular([0, 0], 2, { caption: 'My Caption'}),
+`\\begin{tabular}{ll}
+\\multicolumn{2}{c}{My Caption} \\\\ \\hline
+0 & 0
+\\end{tabular}\n`, 'simple 4x4 number matrix');
+    });
+
+    it('should respond with full hLines and vLines on corresponding input', function () {
+      assert.strictEqual(matrix.formattedTabular([0, 0, 0, 0], 2, { vLines: [true, true, true], hLines: [true, true, true]}),
+`\\begin{tabular}{|l|l|}
+\\hline
+0 & 0 \\\\ \\hline
+0 & 0 \\\\ \\hline
+\\end{tabular}\n`);
+    });
+
+    it('should respond with partial hLines on corresponding input', function () {
+      assert.strictEqual(matrix.formattedTabular(new Array(8).fill(0), 3, { hLines: [true, true] }),
+`\\begin{tabular}{lll}
+\\hline
+0 & 0 & 0 \\\\ \\hline
+0 & 0 & 0 \\\\
+0 & 0 &
+\\end{tabular}\n`);
+    });
+
+    it('should respond with partial vLines on corresponding input', function () {
+      assert.strictEqual(matrix.formattedTabular([0, 0, 0, 0], 2, { vLines: [true] }),
+`\\begin{tabular}{|ll}
+0 & 0 \\\\
+0 & 0
+\\end{tabular}\n`);
+    });
+
+    it('should respond with caption and all Lines on corresponding input', function () {
+      assert.strictEqual(matrix.formattedTabular([0, 0, 0, 0], 2, { caption: 'Some Caption', vLines: [true, true, true], hLines: [true, true, true]}),
+`\\begin{tabular}{|l|l|}
+\\multicolumn{2}{c}{Some Caption} \\\\ \\hline
+0 & 0 \\\\ \\hline
+0 & 0 \\\\ \\hline
+\\end{tabular}\n`);
+    });
+  });
+});
